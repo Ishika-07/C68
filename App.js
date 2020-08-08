4a67dbdf5a35215a4461685ba5450e434e2f3038
Ishika-07 <jjyotii82@gmail.com>
2020-08-08 11:06:52 +0530

    Made changes

--- App.js
+++ App.js
@@ -3,25 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-
-import AppHeader from './assets/appheader';
 import StoryScreen from './screens/StoryWritingScreen';
 import SearchScreen from './screens/SearchScreen';
 
 export default class App extends React.Component {
   render(){
     return (
-      <View>
-        <AppHeader/>
-        <AppContainer /> 
-      </View>
+        <AppContainer/> 
     )
   }
 }
 
 const TabNavigator = createBottomTabNavigator({
-  WriteStory: {screen: StoryScreen},
-  ReadStory: {screen: SearchScreen},
+  WriteStory:{screen: StoryScreen},
+  ReadStory:{screen: SearchScreen},
 });
 
 const AppContainer =  createAppContainer(TabNavigator);
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Text, View,TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { Text, View,TouchableOpacity,TextInput } from 'react-native';
+import AppHeader from '../components/appHeader'
 
 export default class Searchscreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{fontSize:20,fontWeight:'bold'}}>Search The Story You Want To Read Over Here</Text>
-          <TextInput style={{width:500, height:300,borderWidth:2,}}/>
+         <View >
+         <AppHeader/>
+         </View>
+         <View>
 
-          <TouchableOpacity style={{backgroundColor:'pink',width:100,height:40,borderWidth:2,borderRadius:10,marginTop:10,justifyContent:'center',alignContent:'center'}}>
+          <Text style={{fontSize:20,fontWeight:'bold',marginTop:50}}>Search The Story You Want To Read Over Here</Text>
+          <TextInput style={{width:500, height:50,borderWidth:2,marginTop:20}}/>
+
+          <TouchableOpacity style={{backgroundColor:'pink',width:100,height:40,borderWidth:2,borderRadius:10,marginTop:10,justifyContent:'center',alignContent:'center',marginLeft:200,marginBottom:400}}>
             <Text  style={{fontSize:15,fontWeight:'bold',marginLeft:25}}>Search </Text>
             </TouchableOpacity>
+            </View>
         </View>
       );
     }
